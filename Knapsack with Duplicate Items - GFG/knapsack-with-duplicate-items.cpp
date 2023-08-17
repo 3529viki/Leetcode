062b3fad128b9a73e20ac0dece24359f119426ec
VIKASH SINGH <87721528+3529viki@users.noreply.github.com>
2023-08-17 06:56:41 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,43 +9,59 @@ using namespace std;
 
 class Solution{
 public:
- vector<vector<int>> dp;
-    int func(int wt[],int val[],int rem_w, int n , int index){
-        //base case
-        if(index >= n)
-        return 0;
+//  vector<vector<int>> dp;
+//     int func(int wt[],int val[],int rem_w, int n , int index){
+    //     //base case
+    //     if(index >= n)
+    //     return 0;
         
-        if(rem_w <= 0){
-        return 0;
-        }
+    //     if(rem_w <= 0){
+    //     return 0;
+    //     }
         
-        if(dp[index][rem_w] != -1)
-            return dp[index][rem_w];
+    //     if(dp[index][rem_w] != -1)
+    //         return dp[index][rem_w];
         
         
-       // recursive case
+    //   // recursive case
        
-       //pick kr rhe hai
-       int op1=INT_MIN;
+    //   //pick kr rhe hai
+    //   int op1=INT_MIN;
        
-       if(wt[index] <= rem_w){
+    //   if(wt[index] <= rem_w){
            
-       op1= val[index] + func( wt, val, rem_w - wt[index],  n ,  index);
-       }
+    //   op1= val[index] + func( wt, val, rem_w - wt[index],  n ,  index);
+    //   }
        
-       //pick nahi kr rhe
-      int op2 = func( wt , val, rem_w , n , index+1);
+    //   //pick nahi kr rhe
+    //   int op2 = func( wt , val, rem_w , n , index+1);
       
-      return dp[index][rem_w]= max(op1 , op2);
+    //   return dp[index][rem_w]= max(op1 , op2);
        
-    }
+    // }
     
-    int knapSack(int N, int W, int val[], int wt[])
-    {
-         vector<vector<int>>dp(N+1,vector<int>(W+1,-1));
-        this->dp=dp;
-        return func( wt, val, W, N , 0);
-      
+    int knapSack(int N, int W, int val[], int wt[]){
+        //  vector<vector<int>>dp(N+1,vector<int>(W+1,-1));
+        // this->dp=dp;
+        // return func( wt, val, W, N , 0);
+    
+
+vector<int> dp (W+1 , INT_MIN);
+dp[0] = 0;
+int ans = 0;
+
+for (int weight = 1; weight <= W; weight++){
+    
+    for (int i = 0; i < N; i++){
+    if (wt[i] <= weight){
+   dp[weight] = max(dp[weight], val[i] + dp[weight-wt[i]]);
+      }
+   }
+   ans = max(ans, dp[weight]);
+}
+
+return ans;
+
     }
 };
 
